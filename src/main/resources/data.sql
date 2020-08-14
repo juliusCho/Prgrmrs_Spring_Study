eb44f5803a809bf0b548e412a9b6c7b42edcd3f3
cho.inhyo <johncrist2000@gmail.com>
2020-08-14 23:48:29 +0900

    jdbc+h2 config / init ddl+dml

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users
+(email, passwd, login_count, last_login_at, create_at)
+VALUES
+('user1@prgrmrs6.com', '1', 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+('user2@prgrmrs6.com', '1', 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+('user3@prgrmrs6.com', '1', 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
