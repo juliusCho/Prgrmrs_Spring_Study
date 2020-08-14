425e600121ecf393965ceae5f65333840b9f771b
cho.inhyo <johncrist2000@gmail.com>
2020-08-14 19:58:50 +0900

    package created by using spring initializeer

---
+++ src/test/java/com/github/prgrms/socialserver/SocialServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.prgrms.socialserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialServerApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
